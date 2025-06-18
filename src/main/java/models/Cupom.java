2875c0ff341b7022b7c6d7132f08bda009d1a29a
ThiagoSchulze <thiago122783@gmail.com>
2025-06-18 20:17:59 -0300

    Testes de Cupom

---
+++ src/main/java/models/Cupom.java
@@ -0,0 +1,34 @@
+package models;
+
+import java.time.LocalDate;
+
+public class Cupom{
+    private int idCupom;
+    private String codigo;
+    private LocalDate dataVencimento;
+    private double porcentagemDesconto;
+
+    public Cupom(int idCupom, String codigo, LocalDate dataVencimento, double porcentagemDesconto){
+        this.idCupom = idCupom;
+        this.codigo = codigo;
+        this.dataVencimento = dataVencimento;
+        this.porcentagemDesconto = porcentagemDesconto;
+    }
+
+    public boolean validarCupom(){
+        if (dataVencimento != null && !dataVencimento.isBefore(LocalDate.now())){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public double getPorcentagemDesconto() {
+        return porcentagemDesconto;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+}
---
+++ src/test/java/CupomTestes.java
@@ -0,0 +1,40 @@
+import models.Cupom;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class CupomTestes {
+
+    @Test
+    public void deveValidarCupomComDataValida() {
+
+        //Arrange
+        Cupom cupom = new Cupom(1, "JUNHO25", LocalDate.of(2025, 6, 30), 0.05);
+
+        //Act
+        boolean resultado = cupom.validarCupom();
+
+        //Assert
+        assertTrue(resultado);
+    }
+
+    @Test
+    public void deveValidarCupomComDataVencida() {
+
+        //Arrange
+        Cupom cupom = new Cupom(2, "PASCOA", LocalDate.of(2025, 4, 30), 0.10);
+
+        //Act
+        boolean resultado = cupom.validarCupom();
+
+        //Assert
+        assertFalse(resultado);
+    }
+
+    
+
+
+}
