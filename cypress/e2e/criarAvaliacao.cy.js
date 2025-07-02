3d90997e869f521e5c676ed1056c01b80138de1d
claraacv <claravanzuitaifc@gmail.com>
2025-07-02 18:22:54 -0300

    criarAvaliacao com testes com bug

--- cypress/e2e/criarAvaliacao.cy.js
+++ cypress/e2e/criarAvaliacao.cy.js
@@ -6,6 +6,41 @@ describe('Comentários inválidos', () => {
         });
     })
 
+    it('comentário inválido - com estrelas', () => {
+        cy.visit('https://www.amazon.com.br/');
+        cy.get('#nav-link-accountList').click();
+        cy.get('input[name="email"]').type('testesudesc@gmail.com');
+        cy.get('#continue').click();
+        cy.get('input[name="password"]').type('TestesUDESC3!', { log: false });
+        cy.get('#signInSubmit').click();
+        cy.get('#nav-link-accountList').should('contain.text', 'Olá');
+
+        cy.visit('https://www.amazon.com.br/Malibu-renasce-Taylor-Jenkins-Reid/dp/8584392165');
+        cy.contains('a.a-button-text', 'Escreva uma avaliação').click();
+
+        cy.get('img[alt*="5"]').click();
+        cy.get('#reviewTitle').type('Ótimo livro');
+        cy.get('input.a-button-input[value="Enviar"]').click();
+        cy.contains('Selecione uma classificação para continuar').should('exist');
+    });
+
+    it('comentário inválido - sem estrelas', () => {
+        cy.visit('https://www.amazon.com.br/');
+        cy.get('#nav-link-accountList').click();
+        cy.get('input[name="email"]').type('testesudesc@gmail.com');
+        cy.get('#continue').click();
+        cy.get('input[name="password"]').type('TestesUDESC3!', { log: false });
+        cy.get('#signInSubmit').click();
+        cy.get('#nav-link-accountList').should('contain.text', 'Olá');
+
+        cy.visit('https://www.amazon.com.br/Malibu-renasce-Taylor-Jenkins-Reid/dp/8584392165');
+        cy.contains('a.a-button-text', 'Escreva uma avaliação').click();
+
+        cy.get('#reviewTitle').type('Ótimo livro');
+        cy.get('input.a-button-input[value="Enviar"]').click();
+        cy.contains('Adicione um vídeo, uma foto ou uma avaliação escrita').should('exist');
+    });
+
     it('comentário sem login', () =>{
         cy.visit('https://www.amazon.com.br/Malibu-renasce-Taylor-Jenkins-Reid/dp/8584392165');
         cy.contains('a.a-button-text', 'Escreva uma avaliação').click();
